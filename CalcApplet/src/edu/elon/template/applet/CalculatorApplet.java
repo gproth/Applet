fe031ac0c1b5eb0425cb36412f23be8ee622b0d8
gavan roth <groth@elon.edu>
2015-10-14 13:52:40 -0400

    complete applet

--- CalcApplet/src/edu/elon/template/applet/CalculatorApplet.java
+++ CalcApplet/src/edu/elon/template/applet/CalculatorApplet.java
@@ -1,5 +1,6 @@
 package edu.elon.template.applet;
 
+import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -10,149 +11,356 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-public class CalculatorApplet extends JApplet{
-    private JFrame frame;
-    private JPanel buttonPanel;
-    private JTextField displayField;
-    private double total;
-    private String input;
-    private String op;
-	
-	public void CalculatorApplet(){
-		
+public class CalculatorApplet extends JApplet {
+	private JFrame frame;
+	private JPanel buttonPanel;
+	private JTextField displayField;
+	private double total;
+	private String input;
+	private String op;
+	private boolean isResult;
+
+	public void CalculatorApplet() {
+		isResult = false;
+		input = "0";
+		total = 0.0;
+		op = "";
+
 	}
-	
-	public JFrame createPane(){
-		
-		
-		return frame;
+
+	public void init() {
+		displayField = new JTextField(input);
+		buttonPanel = createButtons();
+		JPanel main = new JPanel(new GridLayout(2, 0));
+		main.add(displayField);
+		main.add(buttonPanel);
+		add(main);
 	}
-	
-	public JPanel createButtons(){
-		JPanel panel = new JPanel(new GridLayout(4,4));
-		
+
+	public void createPane() {
+
+	}
+
+	public JPanel createButtons() {
+		JPanel panel = new JPanel(new GridLayout(4, 4));
+
 		JButton button0 = new JButton("0");
-		button0.addActionListener(new ActionListener(){
+		button0.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				input = input + 0;
+				if (isResult) {
+					total = 0.0;
+					isResult = false;
+				}
+				if (input == null) {
+					input = "" + 0;
+				} else {
+					input = input + 0;
+				}
+				displayField.setText(input);
+
 			}
-			
+
 		});
 		JButton button1 = new JButton("1");
-		button1.addActionListener(new ActionListener(){
+		button1.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				input = input + 1;
+				if (isResult) {
+					total = 0.0;
+					isResult = false;
+				}
+				if (input == null) {
+					input = "" + 1;
+				} else {
+					input = input + 1;
+				}
+				displayField.setText(input);
 			}
-			
+
 		});
-		
+
 		JButton button2 = new JButton("2");
-		button2.addActionListener(new ActionListener(){
+		button2.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				input = input + 2;
+				if (isResult) {
+					total = 0.0;
+					isResult = false;
+				}
+				if (input == null) {
+					input = "" + 2;
+				} else {
+					input = input + 2;
+				}
+				displayField.setText(input);
 			}
-			
+
 		});
-		
+
 		JButton button3 = new JButton("3");
-		button3.addActionListener(new ActionListener(){
+		button3.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				input = input + 3;
+				if (isResult) {
+					total = 0.0;
+					isResult = false;
+				}
+				if (input == null) {
+					input = "" + 3;
+				} else {
+					input = input + 3;
+				}
+				displayField.setText(input);
 			}
-			
+
 		});
-		
+
 		JButton button4 = new JButton("4");
-		button4.addActionListener(new ActionListener(){
+		button4.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				input = input + 4;
+				if (isResult) {
+					total = 0.0;
+					isResult = false;
+				}
+				if (input == null) {
+					input = "" + 4;
+				} else {
+					input = input + 4;
+				}
+				displayField.setText(input);
 			}
-			
+
 		});
-		
+
 		JButton button5 = new JButton("5");
-		button5.addActionListener(new ActionListener(){
+		button5.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				input = input + 5;
+				if (isResult) {
+					total = 0.0;
+					isResult = false;
+				}
+				if (input == null) {
+					input = "" + 5;
+				} else {
+					input = input + 5;
+				}
+				displayField.setText(input);
 			}
-			
+
 		});
-		
+
 		JButton button6 = new JButton("6");
-		button6.addActionListener(new ActionListener(){
+		button6.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				input = input + 6;
+				if (isResult) {
+					total = 0.0;
+					isResult = false;
+				}
+				if (input == null) {
+					input = "" + 6;
+				} else {
+					input = input + 6;
+				}
+				displayField.setText(input);
 			}
-			
+
 		});
-		
+
 		JButton button7 = new JButton("7");
-		button7.addActionListener(new ActionListener(){
+		button7.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				input = input + 7;
+				if (isResult) {
+					total = 0.0;
+					isResult = false;
+				}
+				if (input == null) {
+					input = "" + 7;
+				} else {
+					input = input + 7;
+				}
+				displayField.setText(input);
 			}
-			
+
 		});
-		
+
 		JButton button8 = new JButton("8");
-		
+		button7.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if (isResult) {
+					total = 0.0;
+					isResult = false;
+				}
+				if (input == null) {
+					input = "" + 8;
+				} else {
+					input = input + 8;
+				}
+				displayField.setText(input);
+
+			}
+
+		});
+
 		JButton button9 = new JButton("9");
-		
+		button7.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if (isResult) {
+					total = 0.0;
+					isResult = false;
+				}
+				if (input == null) {
+					input = "" + 9;
+				} else {
+					input = input + 9;
+				}
+				displayField.setText(input);
+			}
+
+		});
+
 		JButton buttonDot = new JButton(".");
-		
+		button7.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if (isResult) {
+					total = 0.0;
+					isResult = false;
+				}
+				if (input == null) {
+					input = "" + ".";
+				} else {
+					input = input + ".";
+				}
+				displayField.setText(input);
+			}
+
+		});
+
 		JButton buttonEquals = new JButton("=");
-		buttonEquals.addActionListener(new ActionListener(){
+		buttonEquals.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				double in = Double.parseDouble(input);
-				if (op.equals("+")){
-					
-				}
-				else if(op.equals("-")){
-					
-				}
-				else if(op.equals("/")){
-					
-				}
-				else{
-					
-				}
+				runCalcs();
+				displayField.setText("" + total);
+				op = "";
+				isResult = true;
+				input = "";
 			}
-			
+
 		});
-		
+
 		JButton buttonPlus = new JButton("+");
-		
-		JButton buttonMinus = new JButton("-");
-		
-		JButton buttonDiv = new JButton("*");
-		
-		JButton buttonMult = new JButton("/");
-		buttonMult.addActionListener(new ActionListener(){
+		buttonPlus.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				
-					total = Double.parseDouble(input);
-					input = "0";
-				
+				if (isResult) {
+					isResult = false;
+				} else {
+					if (total == 0.0) {
+						total = Double.parseDouble(input);
+						System.out.println(total);
+					} else {
+						runCalcs();
+					}
+					op = "+";
+					input = "";
+				}
 			}
-			
+
 		});
-		return buttonPanel;
-		
+
+		JButton buttonMinus = new JButton("-");
+		buttonMinus.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if (isResult) {
+					isResult = false;
+				} else {
+					if (total == 0.0) {
+						total = Double.parseDouble(input);
+					} else {
+						runCalcs();
+					}
+					op = "-";
+					input = "";
+				}
+			}
+
+		});
+
+		JButton buttonDiv = new JButton("/");
+		buttonDiv.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if (isResult) {
+					isResult = false;
+				} else {
+					if (total == 0.0) {
+						total = Double.parseDouble(input);
+					} else {
+						runCalcs();
+					}
+					op = "/";
+					input = "";
+				}
+			}
+
+		});
+
+		JButton buttonMult = new JButton("*");
+		buttonMult.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if (isResult) {
+					isResult = false;
+				} else {
+					if (total == 0.0) {
+						total = Double.parseDouble(input);
+					} else {
+						runCalcs();
+					}
+					op = "*";
+					input = "";
+				}
+			}
+
+		});
+		panel.add(button7);
+		panel.add(button8);
+		panel.add(button9);
+		panel.add(buttonDiv);
+		panel.add(button4);
+		panel.add(button5);
+		panel.add(button6);
+		panel.add(buttonMult);
+		panel.add(button1);
+		panel.add(button2);
+		panel.add(button3);
+		panel.add(buttonMinus);
+		panel.add(button0);
+		panel.add(buttonDot);
+		panel.add(buttonEquals);
+		panel.add(buttonPlus);
+		return panel;
+
 	}
-	
-	
-	public double runCalcs(){
-		return total;
-		
+
+	public void runCalcs() {
+		if (op.equals("+")) {
+			total = total + Double.parseDouble(input);
+		} else if (op.equals("-")) {
+			total = total - Double.parseDouble(input);
+		} else if (op.equals("/")) {
+			total = total / Double.parseDouble(input);
+		} else {
+			total = total * Double.parseDouble(input);
+		}
 	}
 }
